8621dc0410053bb67d0ae70b61923838160afd75
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-12-20 21:48:01 -0500

    Adding additional pytest

--- tests/model/pipelines/test_cloudmask_cnn_pipeline.py
+++ tests/model/pipelines/test_cloudmask_cnn_pipeline.py
@@ -1,33 +1,36 @@
-import sys
 import pytest
-from omegaconf import OmegaConf
-from vhr_cloudmask import CloudMaskConfig
+from vhr_cloudmask import CloudMaskPipeline
 
 
 @pytest.mark.parametrize(
-    "config_filename, expected_experiment_name, expected_experiment_type",
+    "config_filename, output_dir, inference_regex_list, " +
+    "expected_experiment_name, expected_experiment_type",
     [(
         'tests/data/cloudmask_test.yaml',
+        'tests/output',
+        ['tests/data/*.tif'],
         'vhr-cloudmask',
         'cloudmask'
     )]
 )
 def test_cloudmask_pipeline(
             config_filename: str,
+            output_dir: str,
+            inference_regex_list: list,
             expected_experiment_name: str,
             expected_experiment_type: str
         ):
 
     # initialize cloud mask object
-    
+    cloudmask_pipeline = CloudMaskPipeline(
+        config_filename,
+        None,
+        None,
+        output_dir,
+        inference_regex_list
+    )
 
-    #schema = OmegaConf.structured(Config)
-    #conf = OmegaConf.load(filename)
-
-    #try:
-    #    conf = OmegaConf.merge(schema, conf)
-    #except BaseException as err:
-    #    sys.exit(f"ERROR: {err}")
-
-    #assert expected_experiment_name == conf.experiment_name
-    #assert expected_experiment_type == conf.experiment_type
+    assert expected_experiment_name == \
+        cloudmask_pipeline.conf.experiment_name
+    assert expected_experiment_type == \
+        cloudmask_pipeline.conf.experiment_type
